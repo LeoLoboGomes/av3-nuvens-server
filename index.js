02da446a32a8a25cf3877101090fa2f47ac7c84a
Leo Gomes <leo.gomes@seplag.ce.gov.br>
2023-05-31 16:21:58 -0300

    finalize creations of all endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import { createClient } from '@supabase/supabase-js'
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
@@ -29,7 +30,7 @@ app.get('/produtos/:id', async (req, res) => {
   const response = await supabase
     .from('products')
     .select()
-    .eq('id', productId)
+    .eq('id', productId);
 
   if (response.status == 200) {
     res.status(200).send(response.data);
@@ -39,13 +40,13 @@ app.get('/produtos/:id', async (req, res) => {
   }
 });
 
-app.post('/produstos', async (req, res) => {
+app.post('/produtos', bodyParser.json(), async (req, res) => {
   const productName = req.body.name;
-  const productPrice = req.body.price;
+  const productPrice = parseFloat(req.body.price);
 
   const response = await supabase
     .from('products')
-    .insert({ name: productName, price: productPrice })
+    .insert({ name: productName, price: productPrice });
 
   if (response.status == 201) {
     res.status(201).send({ message: "Produto Cadastrado!" });
@@ -55,15 +56,18 @@ app.post('/produstos', async (req, res) => {
   }
 });
 
-app.put('/produstos', async (req, res) => {
-  const productId = req.body.id;
+app.put('/produtos', bodyParser.json(), async (req, res) => {
+  console.log(req.body);
+  const productId = parseInt(req.body.id);
   const productName = req.body.name;
-  const productPrice = req.body.price;
+  const productPrice = parseFloat(req.body.price);
 
   const response = await supabase
     .from('products')
     .update({ name: productName, price: productPrice })
-    .eq('id', productId)
+    .eq('id', productId);
+
+  console.log(response);
 
   if (response.status == 204) {
     res.status(204).send({ message: "Produto Atualizado!" });
@@ -79,7 +83,7 @@ app.delete('/produtos/:id', async (req, res) => {
   const response = await supabase
     .from('products')
     .delete()
-    .eq('id', productId)
+    .eq('id', productId);
 
   if (response.status == 204) {
     res.status(204).send({ message: "Produto Deletado!" });
